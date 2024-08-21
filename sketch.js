76b270b8bc27c59236825e09c84022e512fb589e
PonPonc <gybaez321@gmail.com>
2024-08-21 23:37:38 +0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup(){
+    noCanvas();
+    const model = tf.tensor2d([2,23,4,15,6,6], [2,3], 'int32')
+
+    console.log(model.data().then(
+        function(data){
+            console.log(data);
+        }
+    ));
+}
+
+function draw(){
+
+}
