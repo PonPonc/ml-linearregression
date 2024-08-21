51c6f47d1ef64b8f969e81fde8a2c02697c6b6d9
PonPonc <gybaez321@gmail.com>
2024-08-22 00:06:31 +0800

    moved files to public directory

--- public/sketch.js
+++ public/sketch.js
@@ -1,4 +1,8 @@
+
+
 function setup(){
+    const tf = require('@tensorflow/tfjs');
+
     noCanvas();
     const model = tf.tensor2d([2,23,4,15,6,6], [2,3], 'int32')
 
@@ -7,8 +11,9 @@ function setup(){
             console.log(data);
         }
     ));
-}
-
-function draw(){
 
 }
+
+// function draw(){
+
+// }
