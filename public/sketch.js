ca909dbdbac9dbfa0344a7dd242bd193dae292b9
PonPonc <gybaez321@gmail.com>
2024-09-02 22:24:15 +0800

    feat(algorithm): pushing p5 front-end graph design and initial algorithm

--- public/sketch.js
+++ public/sketch.js
@@ -1,19 +1,53 @@
+let x_vals = [];
+let y_vals = [];
 
+let m, b;
+
+const learningRate = 0.2;
+const optimizer = tf.train.sgd(learningRate)
 
 function setup(){
-    const tf = require('@tensorflow/tfjs');
+    createCanvas(400,400);
+    background(0);
 
-    noCanvas();
-    const model = tf.tensor2d([2,23,4,15,6,6], [2,3], 'int32')
+    m = tf.variable(tf.scalar(random(1)));
+    b = tf.variable(tf.scalar(random(1)));
+}
 
-    console.log(model.data().then(
-        function(data){
-            console.log(data);
-        }
-    ));
+function loss(pred, labels){
+    return pred.sub(labels).squared().mean()
+}
+
+function predict(x_vals){
+    const xs = tensor1d(x_vals);
+    const y_vals = xs.mul(m).add(b)
+    console.log(y_vals.data())
+    return y_vals = xs.mul(m).add(b);
+}
+
+function mousePressed(){
+    
+    let x = map(mouseX, 0, width, 0, 1);
+    let y = map(mouseY, 0, height, 1, 0);
+    
+    x_vals.push(x);
+    y_vals.push(y);
+
 
 }
 
-// function draw(){
+function draw(){
 
-// }
+    const ys = tf.tensor1d(y_vals)
+    optimizer.minimize(() => {loss(predict(x_vals), ys)});
+
+    background(0)
+
+    stroke(255)
+    strokeWeight(4)
+    for (let i = 0; i<x_vals.length; i++){
+        let px = map(x_vals[i], 0, 1, 0, width)
+        let py = map(y_vals[i], 0, 1, height, 1)
+        point(px,py)
+    }
+}
